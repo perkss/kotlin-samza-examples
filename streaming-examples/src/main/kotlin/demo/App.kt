8e50818b25c9e0a026f25b7787563c78dd42d120
perkss <stuart.m.perks@gmail.com>
2020-12-12 13:06:06 +0000

    Add basic Spring Boot.

--- streaming-examples/src/main/kotlin/demo/App.kt
+++ streaming-examples/src/main/kotlin/demo/App.kt
@@ -1,12 +1,23 @@
 package demo
 
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App : CommandLineRunner {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(App::class.java)
+    }
+
+    override fun run(vararg args: String) {
+        logger.info("Running the Samza Application Examples")
+    }
+
 }
 
 fun main(args: Array<String>) {
-    println(App().greeting)
+    runApplication<App>(*args)
 }
